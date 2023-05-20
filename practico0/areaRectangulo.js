27e502cc005d9cd7d62323932db0d560fbcfc244
Jdrking <yorshyaan@gmail.com>
2023-05-20 12:33:14 -0300

    Practico NO finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,11 @@
+let base=5;
+let altura=3;
+let area=base*altura;
+console.log(area);
+
+//parte 2
+
+base=12.5;
+altura=5.33;
+area=base*altura;
+console.log(area);
---
+++ practico0/promedio3.js
@@ -0,0 +1,21 @@
+let n1=5;
+let n2=7;
+let n3=9;
+promedio= (n1+n2+n3)/3;
+console.log(promedio);
+
+//parte 2
+
+n1=-2;
+n2=-3;
+n3=-8;
+promedio=(n1+n2+n3)/3;
+console.log(promedio);
+
+//parte3
+
+n1=-1.2;
+n2=-5.3;
+n3=-3.3;
+promedio=(n1+n2+n3)/3;
+console.log(promedio);
