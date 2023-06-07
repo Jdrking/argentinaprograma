f0862e63a4d643a46f581d633229eac69eead719
Jdrking <yorshyaan@gmail.com>
2023-06-06 23:02:44 -0300

    Tres primeros ejercicios Subidos

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,14 @@
+let nombre;
+nombre="Jorge";
+let apellido="Rey";
+let edad=35;
+
+
+console.log("Hola, "+nombre+" "+apellido+". Tienes "+edad+" años.");
+
+
+
+
+
+
+
---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,11 @@
+let var1=10;
+let var2=12;
+let var3=6;
+let var4=8;
+let suma=var1+var2;
+let resta=var4-var3;
+let resultadoFinal=suma*resta;
+
+let esPar=(resultadoFinal%2)==0;
+
+console.log("Mis variables iniciales fueron: "+var1+", "+var2+", "+var3+" y "+var4+". La respuesta a verificar si el resultado final es par es: "+esPar);
