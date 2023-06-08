cf10dd59f4d9ada4c3438afbed43205701eb7e6b
Jdrking <yorshyaan@gmail.com>
2023-06-08 20:59:25 -0300

    Agrego ultimos dos ejercicios

---
+++ practico1/frutas.js
@@ -0,0 +1,17 @@
+let frutas=[];
+frutas[0]="Melon";
+frutas[1]="Frutilla";
+frutas[2]="Naranja";
+frutas[3]="Durazno";
+frutas[4]="Banana";
+
+let ultima=frutas.at(-1);
+let primera=frutas.at(0);
+
+
+
+
+
+
+console.log("La ultima fruta agregada fue: "+ultima);
+console.log("La primera fruta agregada fue: "+primera);
