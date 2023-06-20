a56dbbd471410d974a461c065f136626608054a6
Jdrking <yorshyaan@gmail.com>
2023-06-20 15:59:21 -0300

    subo ejercicio 4

---
+++ practico2/diasDeLaSemana.js
@@ -0,0 +1,38 @@
+const readlineSync = require("readline-sync");
+
+const numero = readlineSync.question("Ingrese un numero por favor: ");
+
+switch(numero){
+  case "1":
+    console.log("Hoy es lunes");
+    break;
+  case "2":
+    console.log("Hoy es martes");
+    break;
+  case "3":
+    console.log("Hoy es miercoles");
+        break;
+  case "4":
+    console.log("Hoy es jueves");
+    break;
+  case "5":
+    console.log("Hoy es viernes");
+    break;
+  case "6":
+    console.log("Hoy es sabado");
+        break;
+  case "7":
+    console.log("Hoy es domingo");
+        break;    
+  default:
+    console.log("Numero invalido")
+        break;
+}
+
+
+
+
+
+
+
+
