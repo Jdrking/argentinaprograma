d45e098535d86fc609f77217d1f1de46a0915321
Jdrking <yorshyaan@gmail.com>
2023-06-27 20:36:54 -0300

    Subo carpeta practico3

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,20 @@
+
+let i;
+
+for (i=10; i>0; i--) {
+   if(i===10){
+    console.log("Cuenta regresiva 10");
+  i--;
+}
+   
+   console.log(i);
+    
+} if (i===0){
+
+console.log("Lanzamiento!");
+}
+
+
+
+
+
